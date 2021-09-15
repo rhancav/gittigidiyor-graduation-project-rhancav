929e3367d4d2b5b5920b0452bb328e4356ed8967
rhancav <edcavdar@gmail.com>
2021-09-15 22:45:46 +0300

    application post controller

--- loan-application-service/src/main/java/loanapplicationapp/loanapplicationservice/controller/LoanApplicationController.java
+++ loan-application-service/src/main/java/loanapplicationapp/loanapplicationservice/controller/LoanApplicationController.java
@@ -1,9 +1,20 @@
 package loanapplicationapp.loanapplicationservice.controller;
 
+import loanapplicationapp.loanapplicationservice.DTO.request.LoanApplicationRequest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/api/loan-applications")
 public class LoanApplicationController {
+    @PostMapping
+    public ResponseEntity<?> postApplication(@RequestBody @Valid LoanApplicationRequest loanApplicationRequest){
+        System.out.println(loanApplicationRequest);
+        return ResponseEntity.ok(loanApplicationRequest);
+    }
 }
