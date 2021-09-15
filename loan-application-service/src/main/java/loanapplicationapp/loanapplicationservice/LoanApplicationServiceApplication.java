e796350ff2b859864aa5e1561046fde50b867154
rhancav <edcavdar@gmail.com>
2021-09-15 22:48:47 +0300

    swagger implementation

--- loan-application-service/src/main/java/loanapplicationapp/loanapplicationservice/LoanApplicationServiceApplication.java
+++ loan-application-service/src/main/java/loanapplicationapp/loanapplicationservice/LoanApplicationServiceApplication.java
@@ -2,8 +2,10 @@ package loanapplicationapp.loanapplicationservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class LoanApplicationServiceApplication {
 
     public static void main(String[] args) {
