4fff08c083ed06e1f4b0ca2bbcb873460dc579a1
rhancav <edcavdar@gmail.com>
2021-09-22 20:06:30 +0300

    inquiry service api initial

---
+++ credit-score-inquiry-api/src/main/java/dev/creditscoreinquiryapi/CreditScoreInquiryApiApplication.java
@@ -0,0 +1,13 @@
+package dev.creditscoreinquiryapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditScoreInquiryApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CreditScoreInquiryApiApplication.class, args);
+    }
+
+}
---
+++ credit-score-inquiry-api/src/test/java/dev/creditscoreinquiryapi/CreditScoreInquiryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.creditscoreinquiryapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditScoreInquiryApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
