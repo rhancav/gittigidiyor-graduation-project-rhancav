b268f50a4380dc58d5c6017aba65fe9092cd3355
rhancav <edcavdar@gmail.com>
2021-09-26 18:28:16 +0300

    project fix

--- sms-service/src/main/java/dev/smsserviceapi/DTO/response/UnsuccessfulDeliveryResponse.java
+++
@@ -1,9 +0,0 @@
-package dev.smsserviceapi.DTO.response;
-/**
- * @author Erhan Cavdar.
- */
-public class UnsuccessfulDeliveryResponse extends AbstractResponse {
-    public UnsuccessfulDeliveryResponse() {
-        super("Ooops, something went wrong.", false);
-    }
-}
